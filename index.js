976b84dd131410f35c107c4bc1f3438c59444fe6
Devan <devangrose@gmail.com>
2018-01-16 19:41:28 +0000

    Added a few pages, testing things out and reviewing ejs

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var express = require("express");
 var app=express();
 
+
+
 app.listen(8080, function(){
 	console.log("Server has successfully started! Listening on port 8080.");
 });
 
 app.get('/', function(req,res) {
-	res.send('Hello World!');
+	res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing",function(req,res){
+	var thing = req.params.thing;
+	res.send("You fell in love with" + thing);
+});
+
+app.get('/home',function(req,res){
+	res.send("Okay this works now");
 });

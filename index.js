e007f60821874583dc33496b5402b2351ba1b0c9
Devan <devangrose@gmail.com>
2018-01-16 18:54:46 +0000

    Changed server startup message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app=express();
 
 app.listen(8080, function(){
-	console.log("Server has successfully started!");
+	console.log("Server has successfully started! Listening on port 8080.");
 });
 
 app.get('/', function(req,res) {
